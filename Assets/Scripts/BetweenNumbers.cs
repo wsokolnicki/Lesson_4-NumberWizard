c38c414ebf3fcdff59be29a7ac76b584ba582092
Hyrybydysz <wsokolnicki93@gmail.com>
2019-07-02 14:23:03 +0200

    Lesson 4

---
+++ Assets/Scripts/BetweenNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BetweenNumbers : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] Text BetweenNum;
+ 
+    void Start()
+    {
+        BetweenNum.text = min.ToString() + " and " + max.ToString();
+    }
+}	
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] Text guestText;
+
+    int guess;
+
+    void Start ()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressingHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressingLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        guestText.text = guess.ToString();
+    }
+}
